2198a86bd63685ea62babd69f0e31496f728df73
Yahya Tezcan <yahya.tezcan@hotmail.com>
2024-10-19 23:56:11 +0300

    gitignore dosyası eklendi ve gereksiz dosyalar çıkarıldı

---
+++ FlappyBird/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace FlappyBird
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureEdit1 = new DevExpress.XtraEditors.PictureEdit();
+            this.behaviorManager1 = new DevExpress.Utils.Behaviors.BehaviorManager(this.components);
+            this.pictureEdit2 = new DevExpress.XtraEditors.PictureEdit();
+            this.pictureEdit3 = new DevExpress.XtraEditors.PictureEdit();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.startButton = new DevExpress.XtraEditors.SimpleButton();
+            this.stopButton = new DevExpress.XtraEditors.SimpleButton();
+            this.scoreLabelControl = new DevExpress.XtraEditors.LabelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.behaviorManager1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit2.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit3.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureEdit1
+            // 
+            this.pictureEdit1.EditValue = ((object)(resources.GetObject("pictureEdit1.EditValue")));
+            this.pictureEdit1.Location = new System.Drawing.Point(128, 227);
+            this.pictureEdit1.Name = "pictureEdit1";
+            this.pictureEdit1.Properties.Appearance.BackColor = System.Drawing.Color.Transparent;
+            this.pictureEdit1.Properties.Appearance.Options.UseBackColor = true;
+            this.pictureEdit1.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
+            this.pictureEdit1.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Zoom;
+            this.pictureEdit1.Size = new System.Drawing.Size(35, 40);
+            this.pictureEdit1.TabIndex = 0;
+            // 
+            // pictureEdit2
+            // 
+            this.pictureEdit2.EditValue = ((object)(resources.GetObject("pictureEdit2.EditValue")));
+            this.pictureEdit2.Location = new System.Drawing.Point(600, 369);
+            this.pictureEdit2.Name = "pictureEdit2";
+            this.pictureEdit2.Properties.Appearance.BackColor = System.Drawing.Color.Transparent;
+            this.pictureEdit2.Properties.Appearance.Options.UseBackColor = true;
+            this.pictureEdit2.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
+            this.pictureEdit2.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Stretch;
+            this.pictureEdit2.Size = new System.Drawing.Size(60, 191);
+            this.pictureEdit2.TabIndex = 1;
+            // 
+            // pictureEdit3
+            // 
+            this.pictureEdit3.EditValue = ((object)(resources.GetObject("pictureEdit3.EditValue")));
+            this.pictureEdit3.Location = new System.Drawing.Point(600, 0);
+            this.pictureEdit3.Name = "pictureEdit3";
+            this.pictureEdit3.Properties.Appearance.BackColor = System.Drawing.Color.Transparent;
+            this.pictureEdit3.Properties.Appearance.Options.UseBackColor = true;
+            this.pictureEdit3.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
+            this.pictureEdit3.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Stretch;
+            this.pictureEdit3.Size = new System.Drawing.Size(60, 200);
+            this.pictureEdit3.TabIndex = 2;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(10, 10);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 3;
+            this.startButton.Text = "Başlat";
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(100, 10);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 4;
+            this.stopButton.Text = "Durdur";
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // scoreLabelControl
+            // 
+            this.scoreLabelControl.Appearance.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.scoreLabelControl.Appearance.Options.UseFont = true;
+            this.scoreLabelControl.Enabled = false;
+            this.scoreLabelControl.Location = new System.Drawing.Point(10, 50);
+            this.scoreLabelControl.Name = "scoreLabelControl";
+            this.scoreLabelControl.Size = new System.Drawing.Size(54, 23);
+            this.scoreLabelControl.TabIndex = 5;
+            this.scoreLabelControl.Text = "Skor:0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.scoreLabelControl);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.pictureEdit3);
+            this.Controls.Add(this.pictureEdit2);
+            this.Controls.Add(this.pictureEdit1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.behaviorManager1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit2.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit3.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.PictureEdit pictureEdit1;
+        private DevExpress.Utils.Behaviors.BehaviorManager behaviorManager1;
+        private DevExpress.XtraEditors.PictureEdit pictureEdit2;
+        private DevExpress.XtraEditors.PictureEdit pictureEdit3;
+        private System.Windows.Forms.Timer gameTimer;
+        private DevExpress.XtraEditors.SimpleButton startButton;
+        private DevExpress.XtraEditors.SimpleButton stopButton;
+        private DevExpress.XtraEditors.LabelControl scoreLabelControl;
+    }
+}
+
---
+++ FlappyBird/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlappyBird
+{
+    public partial class Form1 : Form
+    {
+        int score = 0;
+        int gravity = 5; // Kuşun düşüş hızı
+        int pipeSpeed = 8; // Boruların sola doğru hareket hızı
+        bool gameStarted = false; // Oyunun başlamış olup olmadığını kontrol etmek için
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            scoreLabelControl.Text = "Skor: " + score;
+            // Kuşun düşüşünü simüle edin
+            pictureEdit1.Top += gravity;
+
+            // Boruların sola doğru hareket etmesini sağlayın
+            pictureEdit2.Left -= pipeSpeed;
+            pictureEdit3.Left -= pipeSpeed;
+
+            // Borular ekranın solundan çıktığında yeniden konumlandırın
+            if (pictureEdit2.Left < -50)
+            {
+                pictureEdit2.Left = 800;
+                pictureEdit3.Left = 800;
+                score++; // Skoru artır
+            }
+
+            // Skoru güncelle
+            scoreLabelControl.Text = "Skor: " + score;
+
+            // Kuşun form dışına çıkıp çıkmadığını kontrol edin
+            if (pictureEdit1.Top < -25 || pictureEdit1.Top > 500)
+            {
+                gameOver();
+            }
+
+            // Kuşun borulara çarpıp çarpmadığını kontrol edin
+            if (pictureEdit1.Bounds.IntersectsWith(pictureEdit2.Bounds) || pictureEdit1.Bounds.IntersectsWith(pictureEdit3.Bounds))
+            {
+                gameOver();
+            }
+        }
+
+        private void gameOver()
+        {
+            gameTimer.Stop(); // Timer'ı durdur
+            scoreLabelControl.Text += " - Oyun Bitti!";
+            gameStarted = false; // Oyunun durduğunu belirt
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (!gameStarted) // Eğer oyun başlamamışsa
+            {
+                ResetGame(); // Oyunu sıfırlayın
+                gameTimer.Start(); // Timer'ı başlat
+                gameStarted = true; // Oyunun başladığını belirt
+            }
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            gameTimer.Stop(); // Timer'ı durdur
+            gameStarted = false; // Oyunun durduğunu belirt
+        }
+
+        private void ResetGame()
+        {
+            // Skor ve pozisyonları sıfırlama
+            score = 0;
+            pictureEdit1.Top = 227; // Kuşun başlangıç yüksekliği
+            pictureEdit2.Left = 800; // Boruların başlangıç konumu
+            pictureEdit3.Left = 800; // Boruların başlangıç konumu
+            scoreLabelControl.Text = "Skor: 0";
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = -10; // Kuşun yukarı hareket eder (yükselme hızı arttı)
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                gravity = 5; // Kuş tekrar düşmeye başlar
+            }
+        }
+    }
+}
